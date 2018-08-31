566468b497023c1a2ad73bacda949eb0fda8f67a
Mikhail <devslashnil@gmail.com>
2018-08-31 21:54:01 +0300

    1st error

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ class BotHandler:
         self.token = token
         self.api_url = "https://api.telegram.org/bot{}/".format(token)
 
-    def get_updates(self, offset=None, timeout=30):
+    def get_updates(self, offset=None, timeout=10):
         method = 'getUpdates'
         params = {'timeout': timeout, 'offset': offset}
         resp = requests.get(self.api_url + method, params)
