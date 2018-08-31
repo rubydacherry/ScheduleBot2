5abb2e4f07edecc712510c4d1e022fe314b11bbc
Mikhail <devslashnil@gmail.com>
2018-08-31 21:45:21 +0300

    1st error

--- bot.py
+++ bot.py
@@ -51,7 +51,8 @@ def main():
         last_update_id = last_update['update_id']
         last_chat_text = last_update['message']['text']
         last_chat_id = last_update['message']['chat']['id']
-        last_chat_name = last_update['message']['chat']['first_name']
+        #last_chat_name = last_update['message']['chat']['first_name']
+        last_chat_name = 'кто-то'
 
         if last_chat_text.lower() in pairs:
             greet_bot.send_message(last_chat_id, 'Пары)0), {}'.format(last_chat_name))
