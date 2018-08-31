a3e21e68906db25c8296509a0c0c67827ddc73c2
Mikhail <devslashnil@gmail.com>
2018-08-31 22:16:42 +0300

    1st error

--- bot.py
+++ bot.py
@@ -49,8 +49,9 @@ def main():
         last_update = greet_bot.get_last_update()
         last_chat_text = last_update['message']['text']
         last_chat_id = last_update['message']['chat']['id']
-        '''
         last_update_id = last_update['update_id']
+        
+        '''
         last_chat_name = last_update['message']['chat']['first_name']
         last_chat_name = 'кто-то'
         '''
